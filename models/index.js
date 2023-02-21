ff6472bad750c157823dac0e32136664df44ec5c
Sheogorath9 <ogarrett189@gmail.com>
2023-02-21 14:45:20 -0500

    completed model index

--- models/index.js
+++ models/index.js
@@ -18,7 +18,23 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id',
+    },
+})
+
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id',
+    },
+  })
+
 
 module.exports = {
   Product,
